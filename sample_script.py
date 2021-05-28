940e2b523766c41bad307cf4a2ab2f42e2c5de32
Anupam Gupta <guptaanupam116@gmail.com>
2021-05-28 05:35:47 +0530

    Removed Some Code

--- sample_script.py
+++ sample_script.py
@@ -4,13 +4,11 @@ import requests
 # print(sys.version)
 print(sys.executable)
 
-def greet(who_to_greet):
-    greeting = 'Hello, {}'.format(who_to_greet)
-    return greeting
+# def greet(who_to_greet):
+#     greeting = 'Hello, {}'.format(who_to_greet)
+#     return greeting
 
-# res = requests.get('https://coreyms.com')
-# print(res.status_code)
+res = requests.get('https://coreyms.com')
+print(res.status_code)
+print(res.ok)
 
-
-# print(greet('World'))
-# print(greet('Abgl'))
